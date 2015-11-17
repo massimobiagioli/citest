534d68316a694dc2f1b0739e3c6cf78e8835c2a2
massimobiagioli <github@massimobiagioli.it>
2015-11-17 18:02:32 +0100

    first commit

---
+++ application/controllers/Dummy.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dummy extends CI_Controller {
+	
+    public function test()
+    {
+            echo "Dummy Test Route";
+    }
+
+    
+}
