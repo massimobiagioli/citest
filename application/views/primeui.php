e409fb093565eb9dcbb6133649c426487b782be6
massimobiagioli <github@massimobiagioli.it>
2015-11-18 13:41:04 +0100

    tolto config.php

--- application/views/primeui.php
+++ application/views/primeui.php
@@ -6,13 +6,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <meta charset="utf-8">
         <title>PrimeUI</title>	
         
-        <script src="<?php echo base_url(); ?>assets/components/jquery/dist/jquery.min.js"></script>
-        <script src="<?php echo base_url(); ?>assets/components/jqueryui/jquery-ui.min.js"></script>        
+        <script src="<?php echo base_url(); ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/bower_components/jqueryui/jquery-ui.min.js"></script>        
         <script src="<?php echo base_url(); ?>assets/components/primeui/primeui-2.1-min.js"></script>
 
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/components/jqueryui/themes/redmond/jquery-ui.min.css">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/components/jqueryui/themes/redmond/theme.css">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/components/primeui/primeui-2.1-min.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/bower_components/jqueryui/themes/redmond/jquery-ui.min.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/bower_components/jqueryui/themes/redmond/theme.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/bower_components/primeui/primeui-2.1-min.css">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/resources/font-awesome-4.4.0/css/font-awesome.min.css" />
         
         <script type="text/javascript">
