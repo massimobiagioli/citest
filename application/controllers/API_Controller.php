ac62375feb458f3439e0fc2cd0a5e4c1fc686db5
massimobiagioli <github@massimobiagioli.it>
2015-12-15 14:42:03 +0100

    CORS

--- application/controllers/API_Controller.php
+++ application/controllers/API_Controller.php
@@ -3,19 +3,38 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 abstract class API_Controller extends CI_Controller {    
     
-    public function read($id) {        
+    public function __construct() {        
+        parent::__construct();     
+        $this->authenticateCORS();
+    }
+    
+    public function load($id) {
+        
     }
     
     public function query($filters) {        
+        
     }
     
     public function insert() {
+        
     }
     
     public function update($id) {
+        
     }
     
-    public function delete($id) {        
+    public function delete($id) {
+        
+    }
+    
+    protected function authenticateCORS() {        
+        header("Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method == "OPTIONS") {
+            die();
+        }
     }
     
 }
--- application/controllers/Soggetto.php
+++ application/controllers/Soggetto.php
@@ -5,27 +5,32 @@ require_once APPPATH . 'controllers/API_Controller.php';
 
 class Soggetto extends API_Controller {    
     
-    public function read($id) {
+    public function load($id) {
+        parent::load($id);
         echo "read - id=$id";        
     }
     
     public function query($filters) {
+        parent::query($filters);
         echo "query - filters=$filters";     
     }
     
     public function insert() {
+        parent::insert();
         $data = print_r($_POST, true);
         echo "insert - data=" . $data ;     
     }
     
     public function update($id) {
+        parent::update($id);
         parse_str(file_get_contents("php://input"), $input);
         $data = print_r($input, true);
         echo "update - id=$id  data=$data" ; 
     }
     
     public function delete($id) {        
+        parent::delete($id);
         echo "delete - id=$id" ;     
-    }
+    }    
     
 }
